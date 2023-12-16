b44809fa855d0719afc23fca2e0da6cfc21c0a09
Matt Turner <matt@fiverings.uk>
2023-12-16 21:21:18 +0000

    x

--- tests/test_climate.py
+++ tests/test_climate.py
@@ -4,12 +4,12 @@ from salusfy import climate
 from .config_adapter import ConfigAdapter
 from .entity_registry import EntityRegistry
 
-from . import config
+from . import mock_config
 
 
 def test_entity_is_registered():
     registry = EntityRegistry()
-    config_adapter = ConfigAdapter(config)
+    config_adapter = ConfigAdapter(mock_config)
     climate.setup_platform(None, config_adapter, add_entities=registry.register, discovery_info=None)
     
     assert len(registry.entities) == 1
