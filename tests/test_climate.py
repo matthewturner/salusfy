584155fe25a94aa000084931b8faf143d48995a4
Matt Turner <matt@fiverings.uk>
2023-12-16 21:13:06 +0000

    Add unit tests

--- tests/test_climate.py
+++ tests/test_climate.py
@@ -1,7 +1,15 @@
 import pytest
 
-# from salusfy import climate
+from salusfy import climate
+from .config_adapter import ConfigAdapter
+from .entity_registry import EntityRegistry
+
+from . import config
 
 
-def test_true():
-    assert True
+def test_entity_is_registered():
+    registry = EntityRegistry()
+    config_adapter = ConfigAdapter(config)
+    climate.setup_platform(None, config_adapter, add_entities=registry.register, discovery_info=None)
+    
+    assert len(registry.entities) == 1
